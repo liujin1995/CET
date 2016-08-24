b12864fb922e1ad2ff094201a4082df84577eff8
liujin1995 <635759472@qq.com>
2016-08-24 14:05:05 +0800

    传错代码
    传错代码

--- cetalldata/daili.py
+++ cetalldata/daili.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python
 # coding=utf-8
-import sys
-from CetTicket import CetTicket
-import json
-        
-if __name__ == '__main__':
-    ct = CetTicket()
-    if len(sys.argv) == 4:
-        province = sys.argv[1].decode('utf8')
-        school = sys.argv[2].decode('utf8')
-        name = sys.argv[3].decode('utf8')
-        result['ticket'] = ct.find_ticket_number(province, school, name, cet_type=CetTicket.CET4)
-    elif len(sys.argv) == 3:
-        ticket = sys.argv[1].decode('utf8')
-        name = sys.argv[2].decode('utf8')
-        result = ct.get_score(ticket, name)
-        result['ticket'] = ticket
-    print json.dumps(result)
+proxies = {
+    "http": "http://54.183.70.60:8083",
+}
